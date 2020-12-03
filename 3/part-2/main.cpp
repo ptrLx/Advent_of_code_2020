08e8f695ee8ce4060dad07103dcee9970e72c2ca
ptrLx <13c@posteo.de>
2020-12-03 09:50:06 +0100

    Dec 3 - 2

--- 3/part-2/main.cpp
+++ 3/part-2/main.cpp
@@ -1,68 +1,55 @@
-#include <algorithm>
 #include <fstream>
 #include <iostream>
-#include <iterator>
-#include <sstream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-class token {
-    int min;
-    int max;
-    char c;
-    string password;
-public:
-    token(const string &line) {
-        string temp;
-        char tempc;
-        stringstream ss(line);
-        getline(ss, temp, '-');
-        min = stoi(temp);
-        getline(ss, temp, ' ');
-        max = stoi(temp);
-        ss.get(c);
-        ss.get(tempc);
-        ss.get(tempc);
-        getline(ss, password);
+int countTrees(const vector<string> &lines, int shiftR, int shiftD) {
+    int pos = 0;
+    int treesCount = 0;
+    for (int i = 0; i < lines.size(); i += shiftD) {
+        pos %= lines[i].size();
+
+        if (lines[i][pos] == '#')
+            treesCount++;
+
+        pos += shiftR;
     }
 
-    bool valid() {
-        if((password[min-1] == c) != (password[max-1] == c))
-            return true;
-        else
-            return false;
-        
-    }
-
-    friend ostream &operator<<(ostream &out, token tk) {
-        out << tk.min << " - " << tk.max << " " << tk.c << ": " << tk.password;
-        return out;
-    }
-
-    ~token() {}
-};
+    return treesCount;
+}
 
 int main() {
-    unsigned int countTokens = 0;
+    vector<string> lines;
 
+    //Fileinput
     fstream inputfile;
     inputfile.open("inputfile", ios::in);
     if (!inputfile) {
         cout << "No such file" << endl;
     } else {
         for (string line; getline(inputfile, line);) {
-            token tk(line);
-            if(tk.valid()) {
-                cout << tk << endl;
-                countTokens++;
-            }
+            lines.push_back(line);
         }
     }
     inputfile.close();
 
-    cout << "Count of valid passwords: " << countTokens << endl;
+    int count[5];
+    long prod = 1;
+
+    count[0] = countTrees(lines, 1, 1);
+    count[1] = countTrees(lines, 3, 1);
+    count[2] = countTrees(lines, 5, 1);
+    count[3] = countTrees(lines, 7, 1);
+    count[4] = countTrees(lines, 1, 2);
+
+    for (int i : count) {
+        cout << i << endl;
+        prod *= i;
+    }
+
+    cout << endl << "Multiplied together the number of trees encounter " << prod << endl;
     
     return 0;
 }
