e3432f9ddac271d0a0d7c4a93a0831d4bd908a42
ptrLx <13c@posteo.de>
2020-12-09 13:16:27 +0100

    Dec 9 - 1

--- 9/part-1/main.cpp
+++ 9/part-1/main.cpp
@@ -7,9 +7,9 @@
 using namespace std;
 
 pair<int, int> findPair(int index, vector<long> &numbers) {
-    for(int i1 = index-25; i1 < index; i1++)
-        for(int i2 = i1+1; i2 < index; i2++)
-            if(numbers[i1] + numbers[i2] == numbers[index])
+    for (int i1 = index - 25; i1 < index; i1++)
+        for (int i2 = i1 + 1; i2 < index; i2++)
+            if (numbers[i1] + numbers[i2] == numbers[index])
                 return {i1, i2};
     throw invalid_argument("Not found");
     return {-1, -1};
@@ -29,7 +29,7 @@ int main() {
     inputfile.close();
 
     //Find pair
-    for(int i = 25; i < numbers.size(); i++)
+    for (int i = 25; i < numbers.size(); i++)
         try {
             pair<int, int> indexes = findPair(i, numbers);
         } catch (invalid_argument e) {
